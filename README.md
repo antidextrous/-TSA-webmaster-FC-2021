370d660807e0bb4f9010c131cd9a56730665d57d
davidfeder12345 <96384369+antidextrous@users.noreply.github.com>
2022-01-07 15:25:07 +0000

    fixed registration page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -TSA-webmaster-FC-2021
+# TSA-webmaster-FC-2021
+
+#Contributers To Project
+
+--> David Feder
+--> Murugan Ayyanar
+--> Josh Brown
+--> Etash Jhanji
+--> Ananth Kashyap
--- script.js
+++ script.js
@@ -38,6 +38,43 @@ class Detail {
     this.animation.onfinish = () => this.onAnimationFinish(false);
     this.animation.oncancel = () => this.isClosing = false;
   }
-  
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => this.isExpanding = false;
+  }
 }
 
+
+onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = '';
+  }
+}
+
+document.querySelectorAll('details').forEach((el) => {
+  new Detail(el);
+});
